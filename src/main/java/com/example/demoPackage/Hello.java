c1ad087ced11831cc226fe7427f86c735fca074d
u35643 <103084470+u35643@users.noreply.github.com>
2022-04-06 13:54:16 +0800

    FEAT
    更新程式

---
+++ src/main/java/com/example/demoPackage/Hello.java
@@ -0,0 +1,8 @@
+package com.example.demoPackage;
+
+public class Hello {
+
+  public void sayHello() {
+    System.out.println("Hello");
+  }
+}
---
+++ src/test/java/com/example/demoPackage/DemoPackageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoPackage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPackageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
